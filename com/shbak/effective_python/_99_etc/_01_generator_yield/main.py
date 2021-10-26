e6132ebed1a6674e2773a586719d2d950bc86d44
sanghyunbak <sanghyunbak@gmail.com>
2021-10-26 20:49:24 +0900

    add iterator's characteristic

--- com/shbak/effective_python/_99_etc/_01_generator_yield/main.py
+++ com/shbak/effective_python/_99_etc/_01_generator_yield/main.py
@@ -1,4 +1,6 @@
 # iterator implements __iter__, __next__또는__getitem__methods
+# iterator can't copy
+# iterator can't reverse (one-way)
 from termcolor import colored
 
 
@@ -8,12 +10,33 @@ def generator_method():
     yield "hihihi"
 
 
-if __name__ == '__main__':
+_it = generator_method()
+
+
+# can't copy generator
+def iterate_with_next_method():
+    print(colored(f'method: {iterate_with_next_method.__name__}', 'yellow'))
+    # it = copy.deepcopy(_it)
+    # it = itertools.tee(_it)
     it = generator_method()
-    print(colored(f'it : {it}', 'green'))
+    print(colored(f'next(it): {next(it)}', 'cyan'))
+    print(colored(f'next(it): {next(it)}', 'cyan'))
+    print(colored(f'next(it): {next(it)}', 'cyan'))
+
+
+def iterate_with___next__():
+    print(colored(f'method: {iterate_with___next__.__name__}()', 'yellow'))
+    # it = itertools.tee(_it)
+    # it = copy.deepcopy(_it)
+    it = generator_method()
+    print(colored(f'it.__next__(): {it.__next__()}', 'cyan'))
+    print(colored(f'it.__next__(): {it.__next__()}', 'cyan'))
+    print(colored(f'it.__next__(): {it.__next__()}', 'cyan'))
+
+
+if __name__ == '__main__':
+    print(colored(f'it : {_it}', 'green'))
     for i in generator_method():
         print(colored(f'{i}', 'blue'))
-    print(colored(f'it.__next__(): {it.__next__()}', 'cyan'))
-    print(colored(f'it.__next__(): {it.__next__()}', 'cyan'))
-    print(colored(f'it.__next__(): {it.__next__()}', 'cyan'))
-    print(colored(f'range(3): {range(3)}', 'magenta'))
+    iterate_with_next_method()
+    iterate_with___next__()
