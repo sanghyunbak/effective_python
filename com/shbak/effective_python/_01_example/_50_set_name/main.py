d35137962bed16f44a7bf386c2d2533e388fe783
sanghyunbak <sanghyunbak@gmail.com>
2021-10-31 22:39:48 +0900

    add chapter50 use __set_name__

---
+++ com/shbak/effective_python/_01_example/_50_set_name/main.py
@@ -0,0 +1,67 @@
+from termcolor import colored
+
+
+class NewField:
+    def __init__(self):
+        self.name = None
+        self.internal_name = None
+
+    def __set_name__(self, owner, name):
+        # when class created, this method is called
+        self.name = name
+        self.internal_name = '_' + name
+
+    def __get__(self, instance, instance_type):
+        if instance is None:
+            return self
+        return getattr(instance, self.internal_name, '')
+
+    def __set__(self, instance, value):
+        setattr(instance, self.internal_name, value)
+
+
+class Field:
+    def __init__(self, name):
+        self.name = name
+        self.internal_name = '_' + self.name
+
+    def __get__(self, instance, instance_type):
+        if instance is None:
+            return self
+        return getattr(instance, self.internal_name, '')
+
+    def __set__(self, instance, value):
+        setattr(instance, self.internal_name, value)
+
+
+class FixedCustomer:
+    first_name = NewField()
+    last_name = NewField()
+    prefix = NewField()
+    suffix = NewField()
+
+
+def run_fixed_customer():
+    cust = FixedCustomer()
+    print(colored(f'[Before]: {cust.first_name!r} {cust.__dict__}'))
+    cust.first_name = 'Mersen'
+    print(colored(f'[After]: {cust.first_name!r} {cust.__dict__}'))
+
+
+class Customer:
+    first_name = Field('first_name')
+    last_name = Field('last_name')
+    prefix = Field('prefix')
+    suffix = Field('suffix')
+
+
+def use_customer():
+    cust = Customer()
+    print(colored(f'[Before]: {cust.first_name!r} {cust.__dict__}', 'green'))
+    cust.first_name = 'euclid'
+    print(colored(f'[After]: {cust.first_name!r} {cust.__dict__}', 'green'))
+
+
+if __name__ == '__main__':
+    use_customer()
+    run_fixed_customer()
