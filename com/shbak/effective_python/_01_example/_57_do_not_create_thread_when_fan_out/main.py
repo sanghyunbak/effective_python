154c4c109660c8a276c9d11105c589e4a3d01244
sanghyunbak <sanghyunbak@gmail.com>
2021-11-02 22:42:38 +0900

    add chapter57 use lock

---
+++ com/shbak/effective_python/_01_example/_57_do_not_create_thread_when_fan_out/main.py
@@ -0,0 +1,41 @@
+from threading import Lock, Thread
+
+from com.shbak.effective_python._01_example._56_when_need_concurrent.main import Grid, step_cell
+
+ALIVE = '*'
+EMPTY = '-'
+
+
+class LockingGrid(Grid):
+    def __init__(self, height, width):
+        super().__init__(height, width)
+        self.lock = Lock()
+
+    def __str__(self):
+        with self.lock:
+            return super().__str__()
+
+    def get(self, y, x):
+        with self.lock:
+            return super().get(y, x)
+
+    def set(self, y, x, state):
+        with self.lock:
+            return super().set(y, x, state)
+
+
+def simulated_threaded(grid):
+    next_grid = LockingGrid(grid.height, grid.width)
+
+    threads = []
+    for y in range(grid.height):
+        for x in range(grid.width):
+            args = (y, x, grid.get, next_grid.set)
+            thread = Thread(target=step_cell, args=args)
+            thread.start()  # fan out
+            threads.append(thread)
+
+    for thread in threads:
+        thread.join()  # fan in
+
+    return next_grid
