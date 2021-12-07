82d74273edec413e3e2b836fa61fa9f0666b63d3
sanghyunbak <sanghyunbak@gmail.com>
2021-12-08 08:02:07 +0900

    check python lists's methods: keys(), values(), items()

--- com/shbak/effective_python/_01_example/_15_dictionary_order/main.py
+++ com/shbak/effective_python/_01_example/_15_dictionary_order/main.py
@@ -7,3 +7,7 @@ _baby_names = {
 
 if __name__ == '__main__':
     print(colored(f'_baby_names: {_baby_names}', 'green'))
+    print(colored(f'============== Return Lists ============', 'magenta'))
+    print(colored(f'_baby_names.keys(): {_baby_names.keys()}', 'green'))
+    print(colored(f'_baby_names.values(): {_baby_names.values()}', 'green'))
+    print(colored(f'_baby_names.items(): {_baby_names.items()}', 'green'))
