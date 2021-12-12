e35d880c47e988607e4a11aa6eb6b62ae887688a
sanghyunbak <sanghyunbak@gmail.com>
2021-12-12 20:44:41 +0900

    check python lists's methods: keys(), values(), items()

--- com/shbak/effective_python/_01_example/_15_dictionary_order/main.py
+++ com/shbak/effective_python/_01_example/_15_dictionary_order/main.py
@@ -5,9 +5,18 @@ _baby_names = {
     'dog': 'puppy'
 }
 
+
+def my_func(**kwargs):
+    print(colored(f'type(kwargs): {type(kwargs)}', 'green'))
+    for key, value in kwargs.items():
+        print('%s = %s' % (key, value))
+
+
 if __name__ == '__main__':
     print(colored(f'_baby_names: {_baby_names}', 'green'))
-    print(colored(f'============== Return Lists ============', 'magenta'))
     print(colored(f'_baby_names.keys(): {_baby_names.keys()}', 'green'))
+    print(colored(f'type(_baby_names.keys()): {type(_baby_names.keys())}', 'green'))
     print(colored(f'_baby_names.values(): {_baby_names.values()}', 'green'))
     print(colored(f'_baby_names.items(): {_baby_names.items()}', 'green'))
+    print(colored(f'_baby_names.items(): {list(_baby_names.items())}', 'green'))
+    my_func(a='a', b='b')
